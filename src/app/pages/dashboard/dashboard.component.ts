daa0b15e5ed5baa39874c14ea3fb2b5a18ed5ecb
varunnsharma2004 <varunnsharma1084@gmail.com>
2025-03-11 09:33:01 +0530

    second

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -14,15 +14,5 @@ export class DashboardComponent {
 constructor(private data:DataService){
   this.data.movies.subscribe(rs=>this.movieData=rs);
 }
-rows = [
-  { name: 'John Doe', age: 25, company: 'Google' },
-  { name: 'Jane Smith', age: 30, company: 'Microsoft' },
-  { name: 'Bob Johnson', age: 35, company: 'Apple' }
-];
-
-columns = [
-  { prop: 'name' },
-  { name: 'Age', prop: 'age' },
-  { name: 'Company', prop: 'company' }
-];
+ 
 }
